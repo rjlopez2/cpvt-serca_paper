8cd7bf48aece0e561982e114056c81c6578e8ff7
Ruben Lopez <rjlopez2@gmail.com>
2022-04-08 11:04:43 +0200

    keeping only bare minimum package list for installation

--- code/install_packages.R
+++ code/install_packages.R
@@ -1,9 +1,4 @@
-pacman::p_load(AnlysisOfWaves, # custom functions for analysis
-               readxl, # read excel files
-               # tidyverse, # data wrangling
-               ggpubr, ggbeeswarm, ggnewscale, colorspace, cowplot,# for data visualization
-               here, # folder's system navigation,
-               # lme4,
+devtools::install_github("rjlopez2/AnlysisOfWaves") # install package containing custom functions for analysis
+pacman::p_load(here, # folder's system navigation,
                broom.mixed) # tools for mixed effect model computation
 
-devtools::install_github("rjlopez2/AnlysisOfWaves") # install package containing custom functions for analysis
