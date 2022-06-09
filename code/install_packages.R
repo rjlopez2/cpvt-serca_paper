796f19cddbc715cb5f7a108dbea63cea100d1e02
Ruben Lopez <rjlopez2@gmail.com>
2022-06-09 15:57:51 +0200

    Delete install_packages.R
    remove no needed file for installing dependencies

--- code/install_packages.R
+++
@@ -1,4 +0,0 @@
-devtools::install_github("rjlopez2/AnlysisOfWaves") # install package containing custom functions for analysis
-pacman::p_load(here, # folder's system navigation,
-               broom.mixed) # tools for mixed effect model computation
-
